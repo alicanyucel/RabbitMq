e722a3e2647fe3bbcd50082bff50951d464ab66a
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-01-25 22:40:19 +0300

    RabbitmqExample

---
+++ Mesajyakala/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace NesajYakala
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://peorsfgb:uTDp6Yxo5VUu0_cec-CqaSboXY0ZpUGX@toad.rmq.cloudamqp.com/peorsfgb");
+            using  var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("mesaj-kuryk", true, false, false);
+            var consumer=new EventingBasicConsumer(channel);
+            channel.BasicConsume("mesaj-kuyruk",true,consumer);
+            consumer.Received += Consumer_Received;
+            Console.ReadKey();
+        }
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e) {
+            Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+
+        }
+    }
+}
---
+++ RabbitmqExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+namespace RabbitmqExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++) {
+
+                MesajGonder();
+            }
+        }
+        public  static void MesajGonder()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://peorsfgb:uTDp6Yxo5VUu0_cec-CqaSboXY0ZpUGX@toad.rmq.cloudamqp.com/peorsfgb");
+            var connection= factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("mesaj-kuyruk",true,false,false);
+            var mesaj = "merhaba ben ali can yücel";
+            var body=Encoding.UTF8.GetBytes(mesaj);
+            Console.ReadKey();
+
+        }
+    }
+}
